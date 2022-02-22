542c64125f7bd5a3f7ec24597698e5532f2d7e51
Ítalo Braga <itojk2@hotmail.com>
2022-02-22 10:54:01 -0300

    fixes: carousel settings

---
+++ scripts/carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        infinite: false,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 966,
+                settings: {
+                    arrows: false,
+                },
+            },
+        ],
+    });
+});
